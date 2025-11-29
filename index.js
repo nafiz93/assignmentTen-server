2363dc05e455f4ef5d4db125993f07105c1c3963
nafiz93 <nafiz93@gmail.com>
2025-11-29 11:47:09 +0100

    set the client and server synched

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors=require('cors');
 const app = express()
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
  
 
 require('dotenv').config() 
@@ -54,6 +54,15 @@ app.post("/listings", async (req, res) => {
 
 app.get("/listings", async (req, res) => {
     const result = await listingsCollection.find().toArray();
+    res.send(result);
+});
+
+// get the listing by the id
+
+app.get("/listings/:id", async (req, res) => {
+    const id = req.params.id;
+    const query={ _id: new ObjectId(id) };
+    const result = await listingsCollection.findOne(query);
     res.send(result);
 });
 
