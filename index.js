4e39a6b91928147d184b4212f0b9ba11b445b813
nafiz93 <nafiz93@gmail.com>
2025-11-29 01:52:17 +0100

    add the post method and add the data to the database

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.post("/orders", async (req, res) => {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+   
   }
 }
 run().catch(console.dir);
